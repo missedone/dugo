76d676de66d4fb478cf1c543b6aabe936ec32a65
Nick Tan <missedone@gmail.com>
2016-05-31 04:37:32 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+du (Disk Usage) with golang
+====
+
+It's a simple du alike app that prints the size of the folder (and its sub-folders)
---
+++ du.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+)
+
+func diskUsage(currPath string, info os.FileInfo) int64 {
+	size := info.Size()
+
+	if !info.IsDir() {
+		return size
+	}
+
+	dir, err := os.Open(currPath)
+	if err != nil {
+		fmt.Println(err)
+		return size
+	}
+	defer dir.Close()
+
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		size += diskUsage(fmt.Sprintf("%s/%s", currPath, file.Name()), file)
+	}
+
+	if threshold == 0 || size >= threshold {
+		prettyPrintSize(size)
+		fmt.Printf(" => [%s%c]\n", currPath, filepath.Separator)
+	}
+
+	return size
+}
+
+func prettyPrintSize(size int64) {
+	if *humanReadable {
+		switch {
+		case size > 1024*1024*1024:
+			fmt.Printf("%.1fG", float64(size)/(1024*1024*1024))
+		case size > 1024*1024:
+			fmt.Printf("%.1fM", float64(size)/(1024*1024))
+		case size > 1024:
+			fmt.Printf("%.1fK", float64(size)/1024)
+		}
+	} else {
+		fmt.Printf("%d", size)
+	}
+}
+
+var usage = `Usage: dugo [options...] <target_dir>
+
+Options:
+  -h  "Human-readable" output.  Use unit suffixes: Byte, Kilobyte, Megabyte,
+             Gigabyte.
+  -t  threshold of the size, any folders' size larger than the threshold will be print. for example, '1G', '10M', '100K', '1024'
+`
+
+var (
+	humanReadable = flag.Bool("h", false, "human readable unit of size")
+	thresholdStr  = flag.String("t", "", "the threshold for printing the folder size")
+	threshold     int64
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, fmt.Sprintf(usage, runtime.NumCPU()))
+	}
+
+	flag.Parse()
+
+	l := len(*thresholdStr)
+	if l > 0 {
+		t, err := strconv.Atoi(*thresholdStr)
+		if err != nil { // threshold string ends with a unit char
+			i, err := strconv.Atoi((*thresholdStr)[0:(l - 1)])
+			if err != nil {
+				usageAndExit("")
+			}
+
+			switch (*thresholdStr)[l-1:] {
+			case "G":
+				t = i * 1024 * 1024 * 1024
+			case "M":
+				t = i * 1024 * 1024
+			case "K":
+				t = i * 1024
+			}
+		}
+		threshold = int64(t)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if flag.NArg() > 0 {
+		dir = flag.Args()[0]
+	}
+
+	info, err := os.Lstat(dir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	diskUsage(dir, info)
+}
+
+func usageAndExit(msg string) {
+	if msg != "" {
+		fmt.Fprintf(os.Stderr, msg)
+		fmt.Fprintf(os.Stderr, "\n\n")
+	}
+	flag.Usage()
+	fmt.Fprintf(os.Stderr, "\n")
+	os.Exit(1)
+}
